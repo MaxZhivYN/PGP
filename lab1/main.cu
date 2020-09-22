0e01e1ba5fd0f0c671f194ecc21edcbbfd16b0f9
maxzhivyn <maksik1xd@gmail.com>
2020-09-22 20:51:37 +0300

    add max vector

--- lab1/main.cu
+++ lab1/main.cu
@@ -1,13 +1,28 @@
-#include <stdio.h>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
     int n;
-
     cin >> n;
-    printf("Hello world! %d", n);
+    
+    double* arr1 = new double[n];
+    double* arr2 = new double[n];
+    double* res = new double[n];
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr1[i];
+    }
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr2[i];
+    }
+
+    for (int i = 0; i < n; ++i) {
+        res[i] = max(arr1[i], arr2[i]);
+        cout << res[i] << " ";
+    }
 
     return 0;
 }
