171afef9ff0bd0728497ec47601de82077d5c7d6
maxzhivyn <maksik1xd@gmail.com>
2020-09-22 21:02:16 +0300

    test

--- lab1/main.cu
+++ lab1/main.cu
@@ -3,26 +3,41 @@
 
 using namespace std;
 
+__global__ void add(int a, int b, int* c) {
+    *c = a + b;
+}
+
 int main() {
-    int n;
-    cin >> n;
+    // int n;
+    // cin >> n;
     
-    double* arr1 = new double[n];
-    double* arr2 = new double[n];
-    double* res = new double[n];
+    // double* arr1 = new double[n];
+    // double* arr2 = new double[n];
+    // double* res = new double[n];
 
-    for (int i = 0; i < n; ++i) {
-        cin >> arr1[i];
-    }
+    // for (int i = 0; i < n; ++i) {
+    //     cin >> arr1[i];
+    // }
 
-    for (int i = 0; i < n; ++i) {
-        cin >> arr2[i];
-    }
+    // for (int i = 0; i < n; ++i) {
+    //     cin >> arr2[i];
+    // }
 
-    for (int i = 0; i < n; ++i) {
-        res[i] = max(arr1[i], arr2[i]);
-        cout << res[i] << " ";
-    }
+    // for (int i = 0; i < n; ++i) {
+    //     res[i] = max(arr1[i], arr2[i]);
+    //     cout << res[i] << " ";
+    // }
+
+    int c;
+    int* dev_c;
+
+    cudaMalloc((void**) &dev_c, sizeof(int));
+
+    add<<<1, 1>>>(1, 2, dev_c);
+
+    cudaMemcpy(&c, dev_c, sizeof(int));
+
+    cout << c;
 
     return 0;
 }
